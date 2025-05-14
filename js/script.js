af4972e9660a345cea285c2fa10481c89e86f1e7
unknown <christiansean1412@gmail.com>
2025-05-14 14:49:15 +0700

    responsive design

--- js/script.js
+++ js/script.js
@@ -8,21 +8,11 @@ window.addEventListener("scroll", function () {
     }
 });
 
-// Auto Slide
-let slideIndex = 0;
-showSlides();
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const navMenu = document.getElementById("nav-menu");
 
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 4000);
-}
+    menuToggle.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+    });
+});
